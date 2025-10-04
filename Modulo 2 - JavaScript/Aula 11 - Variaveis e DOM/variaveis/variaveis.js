05e33e087dcb1c7fd6297abdfde073cba244feea
AnaJuliaPin1h <anajuliapinheiro517@gmail.com>
2025-10-04 12:39:00 -0300

    .

--- Modulo 2 - JavaScript/Aula 11 - Variaveis e DOM/variaveis/variaveis.js
+++
@@ -1,7 +0,0 @@
-var time="Santos"
-var time2="São Paulo"
-
-//snake case separa as palavras apartir do underline
-var times_paulistas
-//var Camel case, separa as palavras apartir da letra maiuscula
-var timesPaulistas

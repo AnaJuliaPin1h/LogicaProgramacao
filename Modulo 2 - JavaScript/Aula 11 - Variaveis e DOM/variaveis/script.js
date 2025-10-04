79016a57e65bf98df564067216729f6860066730
AnaJuliaPin1h <anajuliapinheiro517@gmail.com>
2025-10-04 12:52:59 -0300

    .

---
+++ Modulo 2 - JavaScript/Aula 11 - Variaveis e DOM/variaveis/script.js
@@ -0,0 +1,14 @@
+console.log("Numero"+ 2+2);
+console.log("Soma" + (2+2))
+console.log('Ola Mundo');
+
+// var = global // pode ser chamada antes de criar a variavel
+var nome = "Ana";
+nome = "Julia";
+console.log(nome)
+
+// constante 
+const pi = 3.14;
+
+// let = local
+let sobrenome = "Pinheiro";
---
+++ Modulo 2 - JavaScript/Aula 11 - Variaveis e DOM/variaveis/variaveis.js
@@ -0,0 +1,9 @@
+var nome = "Ana"
+var nome1 = "Julia"
+
+
+// Snake Case
+var nome_sobrenome = "Ana Pinheiro"
+
+// Camel Case
+var nomeSobrenom = "Silva"
